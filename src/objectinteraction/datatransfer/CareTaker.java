78684b59736c7ee304c920e3d9dea945bfff2dfc
jun159 <baojun159@gmail.com>
2016-10-15 16:07:25 +0800

    Added memento implementation

---
+++ src/objectinteraction/datatransfer/CareTaker.java
@@ -0,0 +1,20 @@
+package objectinteraction.datatransfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareTaker {
+   private List<Memento> mementoList;
+   
+   public CareTaker() {
+	   this.mementoList = new ArrayList<Memento>();
+   }
+
+   public void add(Memento state) {
+	   this.mementoList.add(state);
+   }
+
+   public Memento get(int index) {
+	   return this.mementoList.get(index);
+   }
+}
---
+++ src/objectinteraction/datatransfer/Originator.java
@@ -0,0 +1,25 @@
+package objectinteraction.datatransfer;
+
+public class Originator {
+	private String state;
+	
+	public void setState(String state) {
+		// Set current state
+		this.state = state;
+	}
+	
+	public String getState() {
+		// Get current state
+		return state;
+	}
+	
+	public Memento saveStateToMemento() {
+		// Save current state in CareTaker list
+		return new Memento(state);
+	}
+	
+	public void getStateFromMemento(Memento memento) {
+		// Get state from Memento
+		state = memento.getState();
+	}
+}
